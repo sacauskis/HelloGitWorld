9a943a639bd9ae5ec203d3ae5bb321ada7e59368
Mike Sacauskis <msacauskis@forwardslope.com>
2014-04-24 16:03:25 -0700

    Added second println

--- HelloGitWorld/src/com/fsi/Hello.java
+++ HelloGitWorld/src/com/fsi/Hello.java
@@ -3,7 +3,8 @@ package com.fsi;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");		
+		System.out.println("Hello World!");	
+		System.out.println("Hello World!");	
 	}
 
 }
